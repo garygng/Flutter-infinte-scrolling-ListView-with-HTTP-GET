61847f5c51b42c76d23d4db9d18cf0315d860f42
kaka <minka.gng@gmail.com>
2020-02-17 00:30:56 +0800

    Fetch 5 more images using the scrollcontroller and adding the scrollcontroller.addListener

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,28 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   List<String> dogImages = new List();
+  ScrollController _scrollcontroller = new ScrollController();
 
   @override
   void initState() {
-    // TODO: implement initState
+    // implement initState
     super.initState();
     fetchFive();
+
+    _scrollcontroller.addListener(() {
+      print(_scrollcontroller.position.pixels);
+      if (_scrollcontroller.position.pixels ==
+          _scrollcontroller.position.maxScrollExtent) {
+        // If we are at the bottom of the page
+        fetchFive();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    // implement dispose
+    super.dispose();
   }
 
   @override
@@ -27,6 +43,7 @@ class _MyAppState extends State<MyApp> {
           title: new Text("ListView"),
         ),
         body: ListView.builder(
+          controller: _scrollcontroller,
           itemCount: dogImages.length,
           itemBuilder: (BuildContext context, int index) {
             return Container(
